3d9e540a5c5c775013a14d1e702a1374b1f4bdeb
Vimal Patil <vimalpatil287@gmail.com>
2024-07-31 10:39:41 -0700

    Props Changes

--- Basic-React-App/src/Product.jsx
+++ Basic-React-App/src/Product.jsx
@@ -1,11 +1,32 @@
 import "./Product.css";
+import Price from "./Price.jsx"
 
 
-function Product() {
+function Product({ title, idx }) {
+
+    // const list = features?.map((item) => (
+    //             <li>{item}</li>
+    //         ))
+
+    // let isDiscount = price > 30000 ? "Discount of 5%" : "";
+    // let isDiscount = price > 30000;
+    // let styles = { backgroundColor: isDiscount ? "yellow" : "" };
+    let oldPrices = ["12,49", "11,90", "1,599", "599"];
+    let newPrices = ["8,999", "9,199", "899", "278"];
+    let description = [["8,000 DPI", "5 Programmable buttons"], ["intuitive surface", "designed for ipad pro"], ["designed for ipad pro", "intuitive surface"], ["wireless", "optical orientation"]];
     return (
-        <div className="product">
-            <h3>Product Title</h3>
-            <h5>Product Description</h5>
+
+        <div className="product" >
+            <h4>{title}</h4>
+            <p>{description[idx][0]}</p>
+            <p>{description[idx][1]}</p>
+            <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]} />
+            {/* <h3>{title}</h3>
+            <h5>Price: {price}</h5>
+            <p>{features?.map((item) => (
+                <li>{item}</li>
+            ))}</p>
+            {isDiscount && <p>Discount of 5%</p>} */}
         </div>
     );
 }
--- Basic-React-App/src/productTab.jsx
+++ Basic-React-App/src/productTab.jsx
@@ -1,13 +1,23 @@
 import Product from "./Product.jsx";
 
 function productTab() {
-    return (
-        <>
-            <Product />
-            <Product />
-            <Product />
+    // let options = ["hi-tech", "durable", "fast"];
+    // let option2 = { a: "hi-tech", b: "durable", c: "fast" };
+    let styles = {
+        display: "flex",
+        flexwrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+    };
 
-        </>
+    return (
+        <div style={styles}>
+
+            <Product title="Logitech MX Master" idx={0} />
+            <Product title="Apple Pencil (2nd Gen)" idx={1} />
+            <Product title="Zebronics Zeb-transformer" idx={2} />
+            <Product title="Petronics Toad 23" idx={3} />
+        </div>
     );
 }
 
