fe0cb1bb23f4de827cb79a5a720eaa19ef221d45
Vimal Patil <vimalpatil287@gmail.com>
2024-08-05 16:27:30 -0700

    Lottery game changes

--- Basic-React-App/src/App.jsx
+++ Basic-React-App/src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+
 import ProductTab from "./productTab.jsx";
 
 import './App.css'
+import Lottery from './Lottery.jsx';
+import { sum } from "./helper.js";
+
+let winCondition = (ticket) => {
+  return sum(ticket) === 15
+  // return ticket.every((num) => num === ticket[0]);
+  // return ticket[0] === 0;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h2>BlockBluster Deals | Shop Now</h2>
-      <ProductTab />
+      <Lottery n={3} winCondition={winCondition} />
+      {/* <Ticket ticket={[0, 1, 2]} />
+      <Ticket ticket={[5, 6, 7, 7, 8]} /> */}
+      {/* <Lottery /> */}
+      {/* <h2>BlockBluster Deals | Shop Now</h2>
+      <ProductTab /> */}
     </>
   )
 }
